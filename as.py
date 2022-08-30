0221581e8f1f75b906918ec5bd6401b6c8887072
studentpgs <studentatpgs@gmail.com>
2022-08-30 13:05:40 +0530

    committing

---
+++ as.py
@@ -0,0 +1,14 @@
+a = 6
+b = 7
+c = 42
+print ( a == 6)
+print ( a == 7)
+print ( a == 6 and b == 7)
+print ( a == 7 and b == 7)
+print ( not a == 7 and b == 7)
+print (a==7 or b==7)
+print (a==7 or b==7)
+print ( a==7 or b==6)
+print ( not (a == 7 and b == 6))
+print ( not a == 7 and b == 6)
+
